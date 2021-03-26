0bdfd38d442fb24a41ba4212cf2411eb63dabcd3
Pierre COUCHENEY <pierre.coucheney@uvsq.fr>
2021-03-26 10:51:02 +0100

    règle du jeu de la vie implémentée

--- jeu_de_la_vie.py
+++ jeu_de_la_vie.py
@@ -33,10 +33,9 @@ NB_LIG = HAUTEUR // COTE
 
 tableau = []
 
-
-
 ##########################
 # fonctions du programme
+
 
 def quadrillage():
     """Dessine un quadrillage formé de carrés de côté COTE"""
@@ -49,8 +48,10 @@ def quadrillage():
         canvas.create_line((x, 0), (x, HAUTEUR), fill=COUL_QUADR)
         x += COTE
 
+
 def xy_to_ij(x, y):
-    """Retourne les coordonnées de la case du tableau correspondant au pixel de coordonnées (x, y)"""
+    """Retourne les coordonnées de la case du tableau
+    correspondant au pixel de coordonnées (x, y)"""
     return x // COTE, y // COTE
 
 
@@ -58,9 +59,11 @@ def chg_case(event):
     """Change l'état de la case sur laquelle on a cliqué"""
     i, j = xy_to_ij(event.x, event.y)
     if tableau[i][j] == 0:
-        #si la case est morte
+        # si la case est morte
         x, y = i * COTE, j * COTE
-        carre = canvas.create_rectangle((x, y), (x+COTE, y+COTE), fill=COUL_CARRE, outline=COUL_QUADR)
+        carre = canvas.create_rectangle((x, y),
+                                        (x+COTE, y+COTE),
+                                        fill=COUL_CARRE, outline=COUL_QUADR)
         tableau[i][j] = carre
     else:
         canvas.delete(tableau[i][j])
@@ -68,16 +71,40 @@ def chg_case(event):
 
 
 def nb_vivant(i, j):
-    """Retourne le nombre de cases vivantes autour de la case de coordonnées (i, j)"""
-    return 0
+    """Retourne le nombre de cases vivantes
+       autour de la case de coordonnées (i, j)"""
+    cpt = 0
+    for k in range(max(0, i-1), min(NB_COL, i+2)):
+        for el in range(max(0, j-1), min(NB_LIG, j+2)):
+            if tableau[k][el] != 0 and [k, el] != [i, j]:
+                cpt += 1
+    return cpt
 
 
 def etape_ij(i, j):
-    """Fait une étape du jeu de la vie sur la case de coordonnées (i, j) et retourne la valeur à modifier dans le tableau"""
-    pass
+    """Fait une étape du jeu de la vie sur la case de coordonnées (i, j)
+       et retourne la valeur à modifier dans le tableau"""
+    n = nb_vivant(i, j)
+    if tableau[i][j] == 0:
+        # si la case est morte
+        if n == 3:
+            x, y = i * COTE, j * COTE
+            carre = canvas.create_rectangle((x, y), (x+COTE, y+COTE),
+                                            fill=COUL_CARRE,
+                                            outline=COUL_QUADR)
+            return carre
+        else:
+            return 0
+    else:
+        # si la case est vivante
+        if n in [2, 3]:
+            return tableau[i][j]
+        else:
+            canvas.delete(tableau[i][j])
+            return 0
 
 
-def etape():
+def etape(event):
     """Fait une étape du jeu de la vie"""
     global tableau
     tableau_res = copy.deepcopy(tableau)
@@ -105,10 +132,9 @@ canvas.grid()
 
 # gestion des événements
 canvas.bind("<1>", chg_case)
+racine.bind("n", etape)
 
 # autres fonctions
 quadrillage()
 
 racine.mainloop()
-
-
